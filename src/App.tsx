7144903db4712550962f304989b84b19a1d92fb5
Gary Hill <garyahill.2023@gmail.com>
2024-09-09 15:04:12 -0600

    added proprietary fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
+import "../theme/global.less";
 import './App.less';
-import Heading from './components/heading';
+import Heading from "./components/layout/header";
 
 function App() {
 
 	return (
-		<div className="app-container">
-			<Heading />
-		</div>
+		<>
+			<div className="app-container">
+				<Heading />
+			</div>
+			<div className="app-container">
+				<center><h3>Coming Soon!</h3></center>
+			</div>
+
+		</>
 	);
 }
 
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./header.less";
+
+const Header: React.FC = () => {
+
+	return (
+		<div className={"main-header-container"}>
+			<div className={"flex-container"}>
+				Porter Legacy Group Content
+			</div>
+		</div>
+	);
+};
+
+export default Header;
