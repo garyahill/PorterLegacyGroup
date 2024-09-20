bbf6978ce7e23bb6cbf99d44b3415e791d655f18
Gary Hill <garyahill.2023@gmail.com>
2024-09-19 20:07:51 -0600

    updates to the homepage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,21 @@ const Home: React.FC = () => {
 
 	return (
 		<div className="home-container">
-			Home Page - Coming Soon!
+			<div className="banner">
+				<h1>Porter Legacy is a private, multifamily (MF) real estate investment firm focused on workforce housing in the
+					Dallas-Fort Worth (DFW) area and the State of Texas. We provide carefully curated investment opportunities
+					that offer risk-adjusted, monthly cash flow and capital appreciation with significant tax-advantages for
+					individual accredited and institutional investors.
+				</h1>
+			</div>
+
+			<div className="mission">
+				<h2>Our Mission</h2>
+				<p>Our mission is to excel in the acquisition, renovation, lease, and divestiture of multifamily properties,
+					ensuring consistent returns for investors and building strong relationships with partners in the communities in which we operate.
+				</p>
+			</div>
+
 		</div>
 	);
 };
