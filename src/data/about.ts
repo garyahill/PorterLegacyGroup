48f43c9428ee44815ab60683d8a4a34af6d42268
Gary Hill <garyahill.2023@gmail.com>
2024-10-02 18:26:42 -0600

    updates to the about page

--- src/data/about.ts
+++ src/data/about.ts
@@ -8,42 +8,42 @@ export default function getAboutPageData(): AboutPageData {
 	const valuesItems = [] as ValuesItem[];
 
 	builder = [];
-	builder.push("Prioritize and obsess over customer needs and satisfaction above all else.");
+	builder.push("<span class='special-character'>\"</span>Prioritize and obsess over customer needs and satisfaction above all else.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Customer Obsession",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Meet established objectives by delivering high-quality products or services in a timely and consistent manner.");
+	builder.push("<span class='special-character'>\"</span>Meet established objectives by delivering high-quality products or services in a timely and consistent manner.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Deliver Results",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Build solid relationships based on transparency<span class='special-character'>,</span> honesty and mutual respect.");
+	builder.push("<span class='special-character'>\"</span>Build solid relationships based on transparency<span class='special-character'>,</span> honesty and mutual respect.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Earn Trust",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Maximize available resources and find efficient ways to achieve results without overspending.");
+	builder.push("<span class='special-character'>\"</span>Maximize available resources and find efficient ways to achieve results without overspending.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Frugality",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Prioritize quick decision-making and action over procrastination and over-analysis.");
+	builder.push("<span class='special-character'>\"</span>Prioritize quick decision-making and action over procrastination and over-analysis.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Bias for Action",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Have a bold and ambitious vision that inspires meaningful actions and results.");
+	builder.push("<span class='special-character'>\"</span>Have a bold and ambitious vision that inspires meaningful actions and results.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Think Big",
 		SectionText: builder.join(""),
@@ -51,21 +51,21 @@ export default function getAboutPageData(): AboutPageData {
 
 	///
 	builder = [];
-	builder.push("Set exceptional quality standards and demand their fulfillment in all areas of work.");
+	builder.push("<span class='special-character'>\"</span>Set exceptional quality standards and demand their fulfillment in all areas of work.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Insist on the Highest Standards",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Maintain a mindset of constant learning and always be open to new ideas and perspectives.");
+	builder.push("<span class='special-character'>\"</span>Maintain a mindset of constant learning and always be open to new ideas and perspectives.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Learn and Be Curious",
 		SectionText: builder.join(""),
 	});
 
 	builder = [];
-	builder.push("Encourage innovation and creativity<span class='special-character'>,</span> and seek ways to simplify processes and solutions.");
+	builder.push("<span class='special-character'>\"</span>Encourage innovation and creativity<span class='special-character'>,</span> and seek ways to simplify processes and solutions.<span class='special-character'>\"</span>");
 	valuesItems.push({
 		SectionTitle: "Invent and Simplify",
 		SectionText: builder.join(""),
